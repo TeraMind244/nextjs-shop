a45290daea51dad3178af0e4b732d500444fd5a2
TeraMind244 <teramind244@gmail.com>
2020-10-30 15:14:06 +0700

    Project structure

---
+++ interfaces/Authentication.ts
@@ -0,0 +1,5 @@
+export interface IAuthentication {
+	username: string;
+	authenticationId: string;
+	date: number;
+}
---
+++ interfaces/Shop.ts
@@ -0,0 +1,6 @@
+export interface IProduct {
+	name: string;
+	price: number;
+}
+
+export type IShop = IProduct[];
